9f6d088ab7829a58f0e28f6ea59f57a35ca92633
Marius Atkinson <mak@MAK-MO-MAC.local>
2025-11-03 07:09:42 +0000

    initial implementation

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import split_book_to_words
+from stats import split_book_to_words, letter_counter
 
 
 def main():
-    
-    num_words = split_book_to_words("./books/frankenstein.txt")
+    book = "./books/frankenstein.txt" 
+
+    num_words = split_book_to_words(book)
+    letter_stats = letter_counter(book)
+    letter_stats.sort()
 
     print(f"Found {num_words} total words")
+    
+    for k, v in letter_stats.items():
+        print(f"'{k}': {v}")
 
 main()
--- stats.py
+++ stats.py
@@ -7,6 +7,21 @@ def get_book_text(book_path):
     return book_contents
 
 
+def letter_counter(book_path):
+    book_contents = get_book_text(book_path)
+
+    letter_counter_register = {}
+
+    for char in book_contents.lower():
+        if char.isalpha():
+            if char in letter_counter_register:
+                letter_counter_register[char] += 1
+            else:
+                letter_counter_register[char] =1
+
+    return letter_counter_register
+    
+
 def split_book_to_words(book_path):
 
     book_contents = get_book_text(book_path)
