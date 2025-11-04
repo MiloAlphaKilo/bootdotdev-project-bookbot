f616e8c734de26be8e8b4f050b23e0b33b987540
Marius Atkinson <mak@MAK-MO-MAC.local>
2025-11-04 06:55:16 +0000

    fix - formatting on report

--- report.py
+++ report.py
@@ -3,7 +3,7 @@ from stats import letter_counter, word_counter
 def book_report_header(book_path):
     
     book_report_header = "============ BOOKBOT ============"
-    book_string = "Analyzing book found at {book_path}..."
+    book_string = f"Analyzing book found at {book_path}..."
     book_report_header_string = f"{book_report_header}\n{book_string}\n"
 
     return book_report_header_string
@@ -12,7 +12,7 @@ def book_report_header(book_path):
 def book_report_word_count(book_path):
 
     word_count_header = "----------- Word Count ----------"
-    word_count = word_counter(book_path)
+    word_count = f"Found {word_counter(book_path)} total words"
     word_count_string = f"{word_count_header}\n{word_count}\n"
     
     return word_count_string
@@ -20,7 +20,7 @@ def book_report_word_count(book_path):
 def book_report_letter_counts(book_path):
     
     letter_counts_header = "--------- Character Count -------"
-    letter_count = "\n".join(f"'{k}': {v}" for k, v in letter_counter(book_path).items())
+    letter_count = "\n".join(f"{k}: {v}" for k, v in letter_counter(book_path).items())
     letter_counter_string = f"{letter_counts_header}\n{letter_count}\n"
 
     return letter_counter_string
