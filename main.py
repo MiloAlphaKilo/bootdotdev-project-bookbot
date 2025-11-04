4ac998ca9a5051771612030504b477804cbc4fc3
Marius Atkinson <mak@MAK-MO-MAC.local>
2025-11-04 21:45:17 +0000

    add in argument usage for pook paths
    clean up main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from report import book_report_complete  
+import sys
 
+from report import book_report_complete  
 
 def main():
     
-    book = "./books/frankenstein.txt" 
-    report = book_report_complete(book)
-    
-    print(f"{report}")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = sys.argv[1]
+        report = book_report_complete(book)
+        print(f"{report}")
 
 
 main()
